ba824723676ec058fc6602ae18c5e1e5a063afef
Rafael Costa Braga <c111807@corp.caixa.gov.br>
2017-05-31 20:39:58 -0300

    version 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
